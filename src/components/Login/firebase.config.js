1f941c8ea54e8e70cfc9a7fb7d77208cf7844e87
moin uddin <moinuddincr77@gmail.com>
2021-04-17 19:35:06 +0600

    finish burj al arab project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA048cB9dg-KaWD_i7Lokzef-58D_L3sjg",
+    authDomain: "burj-al-arab-6818d.firebaseapp.com",
+    projectId: "burj-al-arab-6818d",
+    storageBucket: "burj-al-arab-6818d.appspot.com",
+    messagingSenderId: "233418394166",
+    appId: "1:233418394166:web:57825ebab8263db75ba697"
+  };
+
+
+  export default firebaseConfig;
